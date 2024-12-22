886f57c4a3a108f62f06be46c47facd9597f0da9
Litvin Sofia <sofialitvin823@gmail.com>
2024-12-22 14:32:06 +0300

    deleted class Product, constructor for Warehouse

--- main.cpp
+++ main.cpp
@@ -7,37 +7,22 @@
 #include <random>
 #include <windows.h>
 #include <memory>
-class Product {
-private:
-	std::string name_;
-	float price_;
-	float weight_;
-	float priceForKg_;
-public:
-	Product(std::string name, float price, float weight):
-		name_(name),
-		price_(price),
-		weight_(weight),
-		priceForKg_(price*weight)
-	{}
-};
 class Supply {
 private:
-	std::string date_;
-	std::vector<Product> products_;
 	int factoryPriority_;
 public:
-	Supply(std::string date, std::vector<Product> products, int factPriority) :
-		date_(date),
-		products_(products),
+	Supply(int factPriority) :
 		factoryPriority_(factPriority)
 	{}
 };
 class Warehouse {
 private:
-	std::vector<Supply*> warehouse;
-	Supply** pointerToNextInserted;
+	std::vector<Supply*> warehouse{};
+	std::vector<Supply*>::iterator pointerToNextInserted;
 public:
+	Warehouse():
+		pointerToNextInserted(warehouse.begin())
+	{}
 	Supply* chooseSupply() {};
 	Supply** findPlaceForSupply() {};
 	Supply** getPtrToNextInserted() {};
@@ -50,8 +35,8 @@ private:
 	Warehouse* ptrToWarehouse;
 	std::queue<Supply> supplyQueue{};
 public:
-	SystemForPlacingProducts() :
-		supplyQueue()
+	SystemForPlacingProducts(Warehouse* ptr) :
+		ptrToWarehouse(ptr)
 	{}
 	Supply** checkPlaceForSupply() {};
 	bool acceptSupply(Supply sup)
@@ -75,13 +60,13 @@ public:
 		while (true) {
 			std::random_device rd;
 			std::mt19937 gen(rd());
-			std::uniform_int_distribution<> dis(1, 100);
-			int random_number = dis(gen);
+			std::uniform_int_distribution<> dis1(1, 100);
+			int random_number = dis1(gen);
 			Sleep(random_number*10);
-			std::string date = "";
-			std::vector<Product> products{};
-			int factoryPriority = 1;
-			ptrToPlacing_->acceptSupply(Supply{ date, products, factoryPriority });
+			std::normal_distribution<> dis2(1, 1);
+			int factoryPriority = static_cast<int>(dis2(gen));
+			std::cout << factoryPriority << '\n';
+			ptrToPlacing_->acceptSupply(Supply{factoryPriority});
 		}
 	};
 };
@@ -108,7 +93,8 @@ private:
 };
 
 int main(){
-	SystemForPlacingProducts sys;
+	Warehouse h;
+	SystemForPlacingProducts sys(&h);
 	Factory f(1, &sys);
 	std::thread thread1(&Factory::supplyProducts, &f);
 	thread1.join();
